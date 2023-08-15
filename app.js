9aefe8d74499467a75fc359451cb7f08df5a0129
yaozhang182 <yaozhang182@gmail.com>
2023-08-15 20:45:18 +0300

    counting rows in database

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { serve } from "./deps.js";
 import { configure, renderFile } from "./deps.js";
+import { sql } from "./database/database.js";
 
 configure({
   views: `${Deno.cwd()}/views/`,
@@ -15,14 +16,22 @@ const data = {
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
-  if (url.pathname === "/visits") {
+  if (url.pathname === "/count") {
     data.count++;
     return new Response(await renderFile("count.eta", data), responseDetails);
-  } else if (url.pathname === "/meaning") {
-    return new Response("Seeking truths beyond meaning of life, you will find 43.")
   }
 
-  return new Response("Nothing here yet.");
+  if (url.pathname === "/addresses") {
+    const rows = await sql`SELECT COUNT(*) as count FROM addresses`;
+    let rowCount = -42;
+    if (rows.length > 0) {
+      rowCount = rows[0].count;
+    }
+
+    return new Response(`Total rows: ${rowCount}`);
+  }
+
+  return new Response("Hello you!");
 };
 
 serve(handleRequest, { port: 7777 });
